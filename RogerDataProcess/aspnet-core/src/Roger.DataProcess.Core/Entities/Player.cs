f673e53cc3bc76fdaaa3649d19e44f11f2d8ce27
chao1017 <chaoshuangjui@gmail.com>
2020-03-11 15:35:10 +0800

    建立實體 Entity(練習)

---
+++ RogerDataProcess/aspnet-core/src/Roger.DataProcess.Core/Entities/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+
+namespace Roger.DataProcess.Entities
+{
+    public class Player : Entity<long>, IHasCreationTime
+    {
+        public const string DefaultPlayerName = "DefaultPlayer";
+
+        public virtual string PlayerName { get; set; }
+
+        public virtual long MapID { get; set; }
+
+        public virtual DateTime CreationTime { get; set; }
+
+        public virtual Map Map { get; set; }
+        public Player()
+        {
+            CreationTime = DateTime.Now;
+            MapID = 1;
+        }
+    }
+}
