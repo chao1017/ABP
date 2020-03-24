37dfe0ba3de646b4a9802ad1a4785fdca996fbdd
chao1017 <chaoshuangjui@gmail.com>
2020-03-24 15:58:08 +0800

    實做Interface, ing

---
+++ ABP101/aspnet-core/src/MyHouse.MyProject.Application/Equipment/EquipService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abp.UI;
+using AutoMapper;
+using MyHouse.MyProject.e
+
+namespace MyHouse.MyProject.Equipment
+{
+    class EquipService
+    {
+    }
+}
