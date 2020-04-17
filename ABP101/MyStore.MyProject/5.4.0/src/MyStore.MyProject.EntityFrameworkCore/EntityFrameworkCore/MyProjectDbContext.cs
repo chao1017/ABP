372d8711832d29cce5c9459446b5d405de047110
chao1017 <chaoshuangjui@gmail.com>
2020-04-17 17:31:03 +0800

    Create Entity

--- ABP101/MyStore.MyProject/5.4.0/src/MyStore.MyProject.EntityFrameworkCore/EntityFrameworkCore/MyProjectDbContext.cs
+++ ABP101/MyStore.MyProject/5.4.0/src/MyStore.MyProject.EntityFrameworkCore/EntityFrameworkCore/MyProjectDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Abp.EntityFrameworkCore;
+using MyStore.MyProject.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace MyStore.MyProject.EntityFrameworkCore
@@ -6,6 +7,7 @@ namespace MyStore.MyProject.EntityFrameworkCore
     public class MyProjectDbContext : AbpDbContext
     {
         //Add DbSet properties for your entities...
+        public DbSet<Task> Tasks { get; set; }
 
         public MyProjectDbContext(DbContextOptions<MyProjectDbContext> options) 
             : base(options)
