88b4976e8876dedcc5474e2b68dc2054558b4b39
rogerchao <shuang_jui_chao@hotmail.com>
2020-04-28 23:53:23 +0800

    Testing the TaskAppService

--- ABP101/SimpleTaskSystem/test/Roger.SimpleTaskSystem.Tests/Tasks/TaskAppService_Tests.cs
+++ ABP101/SimpleTaskSystem/test/Roger.SimpleTaskSystem.Tests/Tasks/TaskAppService_Tests.cs
@@ -10,5 +10,30 @@ namespace Roger.SimpleTaskSystem.Tests.Tasks
     public class TaskAppService_Tests : SimpleTaskSystemTestBase
     {
         private readonly ITaskAppService _taskAppService;
+
+        public TaskAppService_Tests()
+        {
+            _taskAppService = Resolve<ITaskAppService>();
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task Should_Get_All_Tasks()
+        {
+            //Act
+            var output = await _taskAppService.GetAll(new GetAllTasksInput());
+
+            //Assert
+            output.Items.Count.ShouldBe(2);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task Should_Get_Filtered_Tasks()
+        {
+            //Act
+            var output = await _taskAppService.GetAll(new GetAllTasksInput { State = TaskState.Open });
+
+            //Assert
+            output.Items.ShouldAllBe(t => t.State == TaskState.Open);
+        }
     }
 }
