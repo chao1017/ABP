6aa8266a440b785d552df8ddcede63b43a48ae9d
roger.chao <roger.chaotradevan.com.tw>
2020-05-06 09:53:20 +0800

    Task List Page

---
+++ ABP101/SimpleTaskSystem/src/Roger.SimpleTaskSystem.Web/Controllers/TasksController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Roger.SimpleTaskSystem.Tasks;
+using Roger.SimpleTaskSystem.Tasks.Dtos;
+using Roger.SimpleTaskSystem.Web.Models.Tasks;
+
+namespace Roger.SimpleTaskSystem.Web.Controllers
+{
+    public class TasksController : SimpleTaskSystemControllerBase
+    {
+        private readonly ITaskAppService _taskAppService;
+
+        public TasksController(ITaskAppService taskAppService)
+        {
+            _taskAppService = taskAppService;
+        }
+
+        public async Task<ActionResult> Index(GetAllTasksInput input)
+        {
+            var output = await _taskAppService.GetAll(input);
+            var model = new IndexViewModel(output.Items);
+            return View(model);
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ ABP101/SimpleTaskSystem/src/Roger.SimpleTaskSystem.Web/Models/Tasks/IndexViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Roger.SimpleTaskSystem.Tasks;
+using Roger.SimpleTaskSystem.Tasks.Dtos;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Roger.SimpleTaskSystem.Web.Models.Tasks
+{
+    public class IndexViewModel
+    {
+        public IReadOnlyList<TaskListDto> Tasks { get; }
+
+        public IndexViewModel(IReadOnlyList<TaskListDto> tasks)
+        {
+            Tasks = tasks;
+        }
+
+        public string GetTaskLabel(TaskListDto task)
+        {
+            switch (task.State)
+            {
+                case TaskState.Open:
+                    return "label-success";
+                //case TaskState.Completed:
+                //    break;
+                default:
+                    return "label-default";
+            }
+        }
+    }
+}
