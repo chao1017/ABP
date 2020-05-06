04755c1e5f0ddeb81a6e1fac35432327fa36a703
roger.chao <roger.chaotradevan.com.tw>
2020-05-06 12:19:41 +0800

    Part1, done

---
+++ ABP101/SimpleTaskSystem/test/Roger.SimpleTaskSystem.Web.Tests/Controllers/TasksController_Tests.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using Roger.SimpleTaskSystem.Tasks;
+using Roger.SimpleTaskSystem.Web.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using AngleSharp.Html.Parser;
+using Xunit;
+
+namespace Roger.SimpleTaskSystem.Web.Tests.Controllers
+{
+    public class TasksController_Tests : SimpleTaskSystemWebTestBase
+    {
+        [Fact]
+        public async System.Threading.Tasks.Task Should_Get_Tasks_By_State()
+        {
+            //Act
+            var response = await GetResponseAsStringAsync(
+                GetUrl<TasksController>(nameof(TasksController.Index), new
+                { state = TaskState.Open })
+            );
+
+            //Assert
+            response.ShouldNotBeNullOrWhiteSpace();
+
+            //Get tasks from database
+            var tasksInDatabase = await UsingDbContextAsync(async dbContext =>
+            {
+                return await dbContext.Tasks
+                    .Where(t => t.State == TaskState.Open)
+                    .ToListAsync();
+            });
+
+            //Parse HTML response to check if tasks in the database are returned
+            var document = new HtmlParser().ParseDocument(response);
+            var listItems = document.QuerySelectorAll("#TaskList li");
+
+            //Check task count
+            listItems.Length.ShouldBe(tasksInDatabase.Count);
+
+            //Check if returned list items are same those in the database
+            foreach (var listItem in listItems)
+            {
+                var header = listItem.QuerySelector(".list-group-item-heading");
+                var taskTitle = header.InnerHtml.Trim();
+                tasksInDatabase.Any(t => t.Title == taskTitle).ShouldBeTrue();
+            }
+        }
+    }
+}
