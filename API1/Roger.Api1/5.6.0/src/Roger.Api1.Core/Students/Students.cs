ba58a90eaa35f780fd4a5c959398ddde236f2125
Roger Chao <chaoshuangjui@gmail.com>
2020-08-04 17:45:56 +0800

    Create Entity

--- API1/Roger.Api1/5.6.0/src/Roger.Api1.Core/Students/Students.cs
+++ API1/Roger.Api1/5.6.0/src/Roger.Api1.Core/Students/Students.cs
@@ -1,12 +1,24 @@
 ﻿using Abp.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Roger.Api1.Students
 {
-    public class Students : Entity
+    [Table("Students")]
+    public class Student : Entity
     { 
-        //
+        [Required]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("Name")]
+        public string Name { get; set; }
+
+        [Column("Age")]
+        public int Age { get; set; }
     }
 }
--- API1/Roger.Api1/5.6.0/src/Roger.Api1.EntityFrameworkCore/EntityFrameworkCore/Api1DbContext.cs
+++ API1/Roger.Api1/5.6.0/src/Roger.Api1.EntityFrameworkCore/EntityFrameworkCore/Api1DbContext.cs
@@ -1,12 +1,13 @@
 ﻿using Abp.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Roger.Api1.Students;
 
 namespace Roger.Api1.EntityFrameworkCore
 {
     public class Api1DbContext : AbpDbContext
     {
         //Add DbSet properties for your entities...
-        //public DbSet<Students> Students { get; set; }
+        public DbSet<Student> Students { get; set; }
 
         public Api1DbContext(DbContextOptions<Api1DbContext> options) 
             : base(options)
