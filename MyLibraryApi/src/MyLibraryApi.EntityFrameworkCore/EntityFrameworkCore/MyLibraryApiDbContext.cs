ec4abe3df941b21a136019e593d36eb15772c148
chao1017 <chaoshuangjui@gmail.com>
2020-10-01 21:26:28 +0800

    建立DBContext

--- MyLibraryApi/src/MyLibraryApi.EntityFrameworkCore/EntityFrameworkCore/MyLibraryApiDbContext.cs
+++ MyLibraryApi/src/MyLibraryApi.EntityFrameworkCore/EntityFrameworkCore/MyLibraryApiDbContext.cs
@@ -1,11 +1,16 @@
 ﻿using Abp.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MyLibraryApi.books;
+using MyLibraryApi.person;
 
 namespace MyLibraryApi.EntityFrameworkCore
 {
     public class MyLibraryApiDbContext : AbpDbContext
     {
         //Add DbSet properties for your entities...
+        public DbSet<Book> books { get; set; }
+
+        public DbSet<Borrower> borrowers { get; set; }
 
         public MyLibraryApiDbContext(DbContextOptions<MyLibraryApiDbContext> options) 
             : base(options)
