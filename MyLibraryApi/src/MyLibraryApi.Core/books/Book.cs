5a85d16e2e6923b9d473de0b36b2ed3aa0e8b9aa
chao1017 <chaoshuangjui@gmail.com>
2020-10-01 21:02:26 +0800

    建立Entity

---
+++ MyLibraryApi/src/MyLibraryApi.Core/books/Book.cs
@@ -0,0 +1,57 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+using MyLibraryApi.person;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MyLibraryApi.books
+{
+
+    public class Book : Entity, IHasCreationTime
+    {
+        /// <summary>
+        /// 書名
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 借閱日
+        /// </summary>
+        public string BorrowDate { get; set; }
+
+        /// <summary>
+        /// 到期日
+        /// </summary>
+        public string DueDate { get; set; }
+
+        /// <summary>
+        /// 借閱證號
+        /// </summary>
+        [Required]
+        public string LibraryCardID { get; set; }
+
+        /// <summary>
+        /// 借閱人
+        /// </summary>
+        [Required]
+        [ForeignKey(nameof(LibraryCardID))]
+        public Borrower BookBorrower { get; set; }
+        
+        
+        public DateTime CreationTime { get; set; }
+
+        public Book()
+        {
+            CreationTime = Clock.Now;
+        }
+
+        public Book(string title)
+        {
+            Title = title;
+        }
+    }
+}
---
+++ MyLibraryApi/src/MyLibraryApi.Core/person/Borrower.cs
@@ -0,0 +1,49 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MyLibraryApi.person
+{
+    [Table("BorrowerInfo")]
+    public class Borrower : Entity, IHasCreationTime
+    {
+        /// <summary>
+        /// 借閱證號
+        /// </summary>
+        [Required]
+        public string LibraryCardID { get; set; }
+
+        /// <summary>
+        /// 借閱人身份證字號
+        /// </summary>
+        public string ID { get; set; }
+
+        /// <summary>
+        /// 借閱人姓
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// 借閱人名
+        /// </summary>
+        public string FirstName { get; set; }
+
+        public DateTime CreationTime { get; set; }
+        
+        public Borrower()
+        {
+            CreationTime = Clock.Now;
+        }
+
+        public Borrower(string last_name, string first_name)
+        {
+            LastName = last_name;
+            FirstName = first_name;
+        }
+    }
+}
