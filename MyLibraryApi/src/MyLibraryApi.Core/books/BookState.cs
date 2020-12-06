fdaae14e6f7b7b52fd0b38fd3eaafaeffe202154
rogerchao <shuang_jui_chao@hotmail.com>
2020-12-06 18:23:16 +0800

    1. 新增欄位 2. 建立interface與實做interface的方法

---
+++ MyLibraryApi/src/MyLibraryApi.Core/books/BookState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibraryApi.books
+{
+    public enum BookState : byte
+    { 
+        CheckedOut = 0,
+        Returned = 1
+    }
+}
